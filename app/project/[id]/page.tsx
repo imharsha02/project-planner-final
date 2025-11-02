b1b62114384d8f3f6f0f790afaa56ac0769f6cba
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-MacBook-Air.local>
2025-11-02 14:53:05 +0530

    Rendering individual projects on click

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,15 +1,24 @@
-import { TypographyH2 } from "@/app/components/Typography/TypographyH2";
 import React from "react";
-
-const ProjectId = () => {
+import { getProjects } from "@/app/lib/getProjects";
+import { TypographyH2 } from "@/app/components/Typography/TypographyH2";
+const ProjectId = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const projects = await getProjects();
+  const project = projects?.find((project) => project.id === id);
+  console.log(project);
   return (
     <div className="a m-10">
-      <TypographyH2 className="tracking-wide border-none text-center">
-        Add steps to track
-      </TypographyH2>
-      <TypographyH2 className="tracking-wide border-none cursor-not-allowed">
-        Or generate with AI
-      </TypographyH2>
+      {/* TODO: Add individual project details */}
+      {project ? (
+        <div>
+          <TypographyH2 className="tracking-wide border-none text-center">
+            {project.project_name}
+          </TypographyH2>
+          {project.department}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
