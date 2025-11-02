599d4d99a35a53263d82b1057d786adce6b4f90c
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-MacBook-Air.local>
2025-11-02 06:42:42 +0530

    Rendering projects from supabase

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,7 +1,42 @@
 import React from "react";
-
-const DashboardPage = () => {
-  return <div>No projects yet</div>;
+import { auth } from "../auth";
+import { redirect } from "next/navigation";
+import { createServerSupabaseServiceClient } from "@/lib/supabase/server";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+const DashboardPage = async () => {
+  const session = await auth();
+  if (!session?.user?.id) {
+    redirect("/");
+  }
+  const supabase = createServerSupabaseServiceClient();
+  const { data: projects, error } = await supabase
+    .from("projects")
+    .select("*")
+    .eq("user_id", session.user.id);
+  if (error) {
+    console.error("Supabase Error:", error);
+    return <div>Error loading projects</div>;
+  }
+  return (
+    <>
+      <div className="flex gap-4">
+        {projects.map((project) => (
+          <div key={project.id} className="w-max mx-auto">
+            <Card className="w-max p-4">
+              <CardHeader>
+                <CardTitle>{project.project_name}</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-2">
+                <p>{project.department}</p>
+                <p>{project.start_date}</p>
+                <p>{project.end_date}</p>
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default DashboardPage;
